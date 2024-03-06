cb9d4f19c1bc75dd303202101f9682b656d6421a
parfaitandre5@gmail.com <parfaitandre5@gmail.com>
2024-03-06 10:56:40 +0100

    drawing feature complete

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
 
-// canvas.width = window.innerWidth
-// canvas.height = window.innerHeight / 2
+//resize canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight / 1.6;
+
+//settings of the canvas
+context.strokeStyle = "#BADASS";
+context.lineJoin = "round";
+context.lineCap = "round";
+context.lineWidth = 20;
+
+//mouse behaviour
+var isDraw = false;
+var lastX = 0, lastY = 0;
+var hue = 0;//rainbaw color
+
+function draw(e) {
+    if (!isDraw) return;
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDraw = true;
+    [lastX, lastY] = [e.clientX, e.offsetY];
+    console.log("fuck");
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDraw = false);
+canvas.addEventListener('mouseout', () => isDraw = false);
+
