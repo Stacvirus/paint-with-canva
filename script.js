514962fefc5060d4c6811b3ccf0fddffcf1eb89c
parfaitandre5@gmail.com <parfaitandre5@gmail.com>
2024-03-07 09:39:17 +0100

    more options with icons added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+import { saveAs } from 'file-saver'
+const save = document.querySelector('.download')
+
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
 const colors = document.querySelector('.colors')
 const thickness = document.querySelector('.bold')
 const eraser = document.querySelector('.eraser')
+const clear = document.querySelector('.clear')
+
+const svgs = document.querySelectorAll('svg')
 
 let isArase = false
-const newColor = {}
+let isRainbow = false
+const newColor = {
+    color: 'black'
+}
 
 //resize canvas
 canvas.width = window.innerWidth;
@@ -16,7 +25,7 @@ canvas.height = window.innerHeight / 1.6;
 context.strokeStyle = "black";
 context.lineJoin = "round";
 context.lineCap = "round";
-context.lineWidth = 20;
+context.lineWidth = 10;
 
 //mouse behaviour
 var isDraw = false;
@@ -25,7 +34,12 @@ var hue = 0;//rainbaw color
 
 function draw(e) {
     if (!isDraw) return;
-    // context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    console.log(isRainbow)
+    if (isRainbow && !isArase) {
+        context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    } else {
+        if (!isArase) context.strokeStyle = newColor.color;
+    }
     context.beginPath();
     context.moveTo(lastX, lastY);
     context.lineTo(e.offsetX, e.offsetY);
@@ -55,12 +69,11 @@ canvas.addEventListener('mouseout', () => isDraw = false);
 //other options
 colors.addEventListener('change', selectOption)
 thickness.addEventListener('change', selectOption)
-eraser.addEventListener('change', () => {
+eraser.addEventListener('click', (e) => {
     console.log('in eraser function')
     isArase = !isArase
     !isArase
-        ? newColor.color
-            ? context.strokeStyle = newColor.color : context.strokeStyle = 'black'
+        ? context.strokeStyle = newColor.color
         : eraserOption()
 })
 
@@ -69,3 +82,34 @@ function eraserOption() {
     context.strokeStyle = "#ffff";
 }
 
+//download your paint
+save.addEventListener('click', () => {
+    canvas.toBlob((blob) => {
+        const imageName = prompt('Enter your paint name: ')
+        saveAs(blob, `${imageName}.png`)
+    })
+})
+
+//show shadow when clicked
+svgs.forEach(btn => {
+    btn.addEventListener('mousedown', showShadow)
+})
+
+
+function showShadow() {
+    console.log(this.title)
+    this.classList.contains('svg')
+        ? this.style.boxShadow = ''
+        : this.style.boxShadow = `0 0 10px 5px ${newColor.color}`
+    this.classList.toggle('svg')
+    !this.classList.contains('stay') && setTimeout(() => {
+        this.style.boxShadow = ``
+    }, 2000);
+
+    if (this.classList.contains('Rainbow')) isRainbow = !isRainbow
+}
+
+//clear the board
+clear.addEventListener('click', () => {
+    window.confirm('I sure you want to clear your work ?') && context.clearRect(0, 0, canvas.width, canvas.height)
+})
